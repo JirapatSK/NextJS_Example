6d2edb8561b7b947b5b4ee37e6c466cc49f90387
jirapat169 <james.jirapat@outlook.com>
2021-01-18 10:24:20 +0700

    Add Preact

--- next.config.js
+++ next.config.js
@@ -1,17 +1,20 @@
+const env = require(`./environments/${process.env.MODE}`);
 const path = require("path");
 const withSass = require("@zeit/next-sass");
 const withCSS = require("@zeit/next-css");
-const env = require(`./environments/${process.env.MODE}`);
+const withPreact = require("next-plugin-preact");
 
-module.exports = withCSS(
-  withSass({
-    basePath: env.basePath,
-    sassOptions: {
-      includePaths: [path.join(__dirname, "styles")],
-    },
-    trailingSlash: true,
-    env: {
-      mode: process.env.MODE,
-    },
-  })
+module.exports = withPreact(
+  withCSS(
+    withSass({
+      basePath: env.basePath,
+      sassOptions: {
+        includePaths: [path.join(__dirname, "styles")],
+      },
+      trailingSlash: true,
+      env: {
+        mode: process.env.MODE,
+      },
+    })
+  )
 );
