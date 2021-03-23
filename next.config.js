4f68bfd9dcb1c1e0b091ff686de3683ce84fbf3a
jirapatsk <james.jirapat@gmail.com>
2021-03-23 17:12:35 +0700

    Update Package.json

--- next.config.js
+++ next.config.js
@@ -1,20 +1,14 @@
 const env = require(`./environments/${process.env.MODE}`);
 const path = require("path");
-const withSass = require("@zeit/next-sass");
-const withCSS = require("@zeit/next-css");
 const withPreact = require("next-plugin-preact");
 
-module.exports = withPreact(
-  withCSS(
-    withSass({
-      basePath: env.basePath,
-      sassOptions: {
-        includePaths: [path.join(__dirname, "styles")],
-      },
-      trailingSlash: true,
-      env: {
-        mode: process.env.MODE,
-      },
-    })
-  )
-);
+module.exports = withPreact({
+  basePath: env.basePath,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  trailingSlash: true,
+  env: {
+    mode: process.env.MODE,
+  },
+});
